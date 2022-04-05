87c37ea268aa8d301aec8f0607cbb74e4973d2ff
Harsh Sonkusare <sonkus_921922@student.nitw.ac.in>
2022-04-05 17:47:59 +0530

    get registration data

--- routes/event.js
+++ routes/event.js
@@ -5,6 +5,7 @@ const { isSignedIn } = require("../controllers/auth");
 const { getAllEvents, addEvent, register_event, getQRCode, checkQRCode, getRegisteredEvents } = require("../controllers/event");
 const upload = require("../utils/upload");
 const feesBharo = require("../models/feesBharo");
+const User = require("../models/user");
 
 router.get("/event/all", getAllEvents);
 router.post("/event/add", upload.single("poster") , addEvent);
@@ -25,5 +26,52 @@ router.get("/getAll/specialEvents",(req, res) => {
         return res.json(fees);
     })
 });
+
+
+router.get("/getRegistrationData", (req, res)=>{
+    User.find({}, (err, users)=>{
+        if(err || !users){
+            return res.status(400).json({
+                error : err
+            });
+        }
+        let registration = 0;
+        let accomodation = 0;
+        let proshow1 = 0;
+        let proshow2 = 0;
+        let proshow3 = 0;
+        users.map((u)=>{
+            const email = u.email;
+            const mail = email.split("@");
+
+            if(mail[1] != "student.nitw.ac.in"){
+                if(u.paidForEvent > 0){
+                    registration += 1;
+                }
+                if(u.paidForAccomodation > 0){
+                    accomodation += 1;
+                }
+                if(u.paidForProshow1 > 0){
+                    proshow1 += 1;
+                }
+                if(u.paidForProshow2 > 0){
+                    proshow2 += 1;
+                }
+                if(u.paidForProshow3 > 0){
+                    proshow3 += 1;
+                }
+                
+            }
+        });
+        return res.json({
+            "No of Registrations" : registration,
+            "Accomodation" : accomodation,
+            "Pro Show 1" : proshow1,
+            "Pro Show 2" : proshow2,
+            "Pro Show 3" : proshow3
+        })
+    });
+});
+
 
 module.exports = router;
